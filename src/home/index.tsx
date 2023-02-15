8f4a2048c42c1a0b091c4db7474961e05d6d129a
Constantino <Okonemmanuelmma@gmail.com>
2023-02-15 15:50:00 +0100

    repositioned tabs

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -10,40 +10,47 @@ function Home() {
     <div className="home-root">
       <div className="home-intro">
         <img src={avatar} />
-        <div className="intro">
-          <div className="name">
-            <p>Okon Emmanuel</p>
+        <div>
+          <div className="intro">
+            <div className="name">
+              <p>Okon Emmanuel</p>
+            </div>
+            <div className="profession">
+              Frontend developer, Blockchain Developer, Web3 enthusiast
+            </div>
+            <div className="prof2">Basically, Beauty and the Blockchain</div>
           </div>
-          <div className="profession">
-            Frontend developer, Blockchain Developer, Web3 enthusiast
+          <div className="bio">
+            <div>Bio</div>
+            <div>
+              I am a frontend developer majoring with the React Ecosystem and
+              web3.js for Web and mobile application development. With 2 years
+              professional experience, I have worked to carve out and fine tune
+              a programming style that allows me replicate a UI design into a
+              functional application, following the best UX philosophies and
+              guidelines for a great web and mobile experience. I am also very
+              invested in the blockchain Ecosystem, and I am currently learning
+              to develop applications for the Ethereum Virtual Machine and other
+              EVM compatible networks with Solidity, and the Near Virtual
+              Machine with Rust(in view) and Assemblyscript.
+            </div>
           </div>
-          <div className="prof2">Basically, Beauty and the Blockchain</div>
-        </div>
-        <div className="bio">
-          <div>Bio</div>
-          <div>
-            I am a frontend developer majoring with the React Ecosystem and
-            web3.js for Web and mobile application development. With 2 years
-            professional experience, I have worked to carve out and fine tune a
-            programming style that allows me replicate a UI design into a
-            functional application, following the best UX philosophies and
-            guidelines for a great web and mobile experience. I am also very
-            invested in the blockchain Ecosystem, and I am currently learning to
-            develop applications for the Ethereum Virtual Machine and other EVM
-            compatible networks with Solidity, and the Near Virtual Machine with
-            Rust(in view) and Assemblyscript.
+          <div className="experience">
+            <div>Professional Experience</div>
+            <div>
+              I have worked in DeFi, EdTech, and Logistics Spanning a 2+ years
+              career as a fronened developer and web3 engineer for several
+              products, Including a DEX pricing oracle querying more than 10
+              dexes, and the DAPP of this ecosystem
+            </div>
+            <div>
+              Download a copy of my resume <a>here</a>
+            </div>
           </div>
-        </div>
-        <div className="experience">
-          <div>Professional Experience</div>
-          <div>
-            I have worked in DeFi, EdTech, and Logistics Spanning a 2+ years
-            career as a fronened developer and web3 engineer for several
-            products, Including a DEX pricing oracle querying more than 10
-            dexes, and the DAPP of this ecosystem
-          </div>
-          <div>
-            Download a copy of my resume <a>here</a>
+          <div className="tabs">
+            <div>Intro</div>
+            <div>Bio</div>
+            <div>Experience</div>
           </div>
         </div>
       </div>
